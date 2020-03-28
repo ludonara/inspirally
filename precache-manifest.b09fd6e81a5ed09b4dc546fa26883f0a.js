9c02f132de18b4083adbbe77a36b6f86ed4207d9
ludonara <l.narayanin@datarocks.io>
2020-03-28 23:26:56 +0400

    Updates

---
+++ precache-manifest.b09fd6e81a5ed09b4dc546fa26883f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4384a51837cfd966c89dc59c85f7b6",
+    "url": "/inspirally/index.html"
+  },
+  {
+    "revision": "ed7b93f66fb2690a5d01",
+    "url": "/inspirally/static/css/main.c45c66f7.chunk.css"
+  },
+  {
+    "revision": "eabfd8552fc757f1f431",
+    "url": "/inspirally/static/js/2.bf0912a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inspirally/static/js/2.bf0912a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7b93f66fb2690a5d01",
+    "url": "/inspirally/static/js/main.f0dd71e2.chunk.js"
+  },
+  {
+    "revision": "bd48292d39e6ac9b05ce",
+    "url": "/inspirally/static/js/runtime-main.52b6e2fc.js"
+  }
+]);
